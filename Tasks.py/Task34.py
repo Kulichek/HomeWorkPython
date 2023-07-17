29766df4a8eb2684df9c7d95869f292b43fb1545
kulichek <kulichek@icloud.com>
2023-07-17 13:40:58 +0300

    винни

--- Tasks.py/Task34.py
+++ Tasks.py/Task34.py
@@ -16,7 +16,7 @@ def ritm(str):
     for word in str:
         sum_w = 0
         for i in word:
-            if i in'рыавопрваоварполимлавьрамо':
+            if i in'хорошо живет на свете винни-пух':
                 sum_w += 1
         list_1.append(sum_w)
     return len(list_1) == list_1.count(list_1[0]) 
